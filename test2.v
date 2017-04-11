aa7b0f297df750b7f690b39962de5471786ba375
youkad <youqad@gmail.com>
2017-04-11 23:42:21 +0200

    update

--- test2.v
+++ test2.v
@@ -42,7 +42,7 @@ Definition count (l:list nat) (x:nat) : nat := fold_left (fun c h => if Nat.eqb 
 
 
 Definition are_shuffled (l:list nat) (l':list nat): Prop 
-  := forall x y, x<y -> x < length l -> y < length l' -> count l x = count l' y.
+  := forall x, count l x = count l' x.
 
 Lemma is_sorted_inv : forall l a, is_sorted (a::l) -> is_sorted l.
 Proof.
@@ -56,23 +56,6 @@ Search "nlt".
 Print Nat.ltb_lt.
 Locate "<?".
 Print lt.
-
-(*
-    pose proof eq_nat_decide.
-    Search eq_nat.
-    pose proof eq_nat_is_eq.
-*)
-(* Lemma well_sorted : forall (l : list nat), is_sorted (sort l). *)
-(* Proof. *)
-(*     intro l. *)
-(*     induction l. *)
-(*      - (* l = nil *) *)
-(*      easy. *)
-(*      - (* l' = a::l *) *)
-(*        simpl. *)
-(*        (* rewrite → IHl. *) *)
-(*        (* reflexivity. *) *)
-(* Admitted. *)
 
 Lemma insert_hd : forall l x, is_sorted l -> hd 0 (insert x l) = x \/ hd 0 (insert x l) = hd 0 l.
 Proof.
@@ -190,9 +173,101 @@ Proof.
   rewrite eqb_refl; rewrite H0.
   simpl.
   apply (count_lemma l 0 x).
- Qed.
+Qed.
 
-Lemma sort_shuffled : forall (l : list nat), are_shuffled (sort l).
+Lemma count_insert2 : forall (x y: nat) (l : list nat), y <> x -> count (insert y l) x = count l x.
 Proof.
-  
+  pose proof Nat.eqb_eq as eqb_eq.
+  pose proof Nat.eqb_neq as eqb_neq.
+  pose proof Nat.eqb_refl as eqb_refl.
+  pose proof Nat.lt_neq as lt_neq.
+  pose proof Nat.ltb_lt as ltb_lt.
+  assert (forall n m : nat, (n <? m) = true -> n <> m) as ltb_neq.
+  intros.
+  apply lt_neq.
+  now apply ltb_lt.
+  clear lt_neq; clear ltb_lt.
+  unfold count.
+  induction l.
+  simpl.
+  intro.
+  apply eqb_neq in H.
+  now rewrite H.
+  intro G.
+  repeat simpl.
+  case_eq (a =? x).
+  intro.
+  simpl.
+  (* apply eqb_eq in H. *)
+  (* rewrite <- H in G; apply eqb_neq in G. *)
+  simpl.
+    case_eq (a <? y).
+    intro.
+    simpl; rewrite H.
+    pose proof (count_lemma l 0 x) as count_lemma1.
+    simpl in count_lemma1.
+    rewrite count_lemma1.
+    pose proof (count_lemma (insert y l) 0 x) as count_lemma2.
+    simpl in count_lemma2.
+    rewrite count_lemma2.
+    now rewrite IHl.
+    intro.
+    simpl; rewrite H.
+    apply eqb_neq in G; rewrite G.
+    now simpl.
+  (* --- *)
+  intro.
+    case_eq (a <? y).
+    intro.
+    simpl; rewrite H.
+    now apply IHl.
+    intro.
+    simpl.
+    rewrite H.
+    apply eqb_neq in G; now rewrite G.
+Qed.
+
+Search "neq".
+
+Lemma sort_shuffled : forall (l : list nat), are_shuffled l (sort l).
+Proof.
+  pose proof Nat.eqb_eq as eqb_eq.
+  pose proof Nat.eqb_neq as eqb_neq.
+  pose proof Nat.eqb_refl as eqb_refl.
+  pose proof Nat.eq_dec as eq_dec.
+  pose proof Nat.eqb_sym as eqb_sym.
+  pose proof Nat.neq_sym as neq_sym.
+  induction l.
+  easy.
+  unfold are_shuffled.
+  simpl.
+  intros.
+  case (eq_dec x a).
+  intro.
+  rewrite e.
+  rewrite count_insert.
+  rewrite <- IHl.
+  unfold count; simpl; rewrite eqb_refl.
+  apply (count_lemma l 0 a).
+  intro.
+  rewrite count_insert2.
+  assert (count (a::l) x = count l x).
+  unfold count.
+  simpl.
+  apply eqb_neq in n; rewrite eqb_sym in n.
+  now rewrite n.
+  rewrite H.
+  now rewrite IHl.
+  now apply neq_sym in n. 
+Qed.
+
+
+Lemma well_sorted : forall (l : list nat), is_sorted (sort l).
+Proof.
+    intro l.
+    induction l.
+    simpl.
+    constructor.
+    simpl.
+    now apply (insert_sort (sort l)) with (x:=a) in IHl.
 Qed.
